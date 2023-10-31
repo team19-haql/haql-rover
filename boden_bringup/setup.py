f228db9ebc23ad1b937c5707bd10efdcf03e3c22
Kai <deweykai5@gmail.com>
2023-10-31 09:57:51 -0600

    init repository

---
+++ boden_bringup/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'boden_bringup'
+
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append(('share/' + package_name + '/launch', ['launch/ekf_launch.py']))
+data_files.append(('share/' + package_name + '/config', ['config/localization.yml']))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='deweykai',
+    maintainer_email='deweykai5@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ webots_dev/launch/robot_launch.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch Webots Tesla driver."""
+
+import os
+import launch
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('webots_dev')
+    world = LaunchConfiguration('world')
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world]),
+        simulation_server_ip='host.docker.internal',
+        ros2_supervisor=True,
+    )
+
+    robot_description_path = os.path.join(package_dir, 'resource', 'rbot.urdf')
+    ros2_control_params = os.path.join(package_dir, 'resource', 'ros2_control.yml')
+    mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'), ('/diffdrive_controller/odom', '/odometry/wheel')]
+    rbot_driver = WebotsController(
+        robot_name='Sojourner',
+        ip_address='host.docker.internal',
+        parameters=[
+            {
+                'robot_description': robot_description_path,
+                'use_sim_time': use_sim_time,
+                'set_robot_state_publisher': True,
+            },
+            ros2_control_params
+        ],
+        remappings=mappings,
+        respawn=True
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': '<robot name=""><link name=""/></robot>'
+        }],
+    )
+
+    footprint_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
+    )
+
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
+    diffdrive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['diffdrive_controller'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+    ros_control_spawners = [diffdrive_controller_spawner, joint_state_broadcaster_spawner]
+
+    # Wait for the simulation to be ready to start the tools and spawners
+    waiting_nodes = WaitForControllerConnection(
+        target_driver=rbot_driver,
+        nodes_to_start=ros_control_spawners,
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='flat_test.wbt',
+            description='Choose one of the world files from `/webots_dev/worlds` directory'
+        ),
+        webots,
+        webots._supervisor,
+        rbot_driver,
+        robot_state_publisher,
+        footprint_publisher,
+        waiting_nodes,
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[
+                    launch.actions.EmitEvent(event=launch.events.Shutdown())
+                ],
+            )
+        )
+    ])
