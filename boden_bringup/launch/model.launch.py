66419ee862c6ab94203c22c2a38e88a9c2290a56
Kai <deweykai5@gmail.com>
2024-03-02 05:56:01 -0700

    add todo

--- boden_bringup/launch/model.launch.py
+++ boden_bringup/launch/model.launch.py
@@ -5,43 +5,53 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, PathJoinSubstitution
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     # Get the package directory
-    description_dir = get_package_share_directory('boden_description')
-    
-    xacro_file = os.path.join(description_dir, 'urdf', 'bodenbot.urdf.xacro')
-    
+    description_dir = get_package_share_directory("boden_description")
+
+    # TODO: update to use new urdf file
+    xacro_file = os.path.join(description_dir, "urdf", "bodenbot.urdf.xacro")
+
     # Launch the robot_state_publisher
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': Command([
-            'xacro', ' ', xacro_file,
-        ])}],
-        remappings=[(
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static'),
-        )]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": Command(
+                    [
+                        "xacro",
+                        " ",
+                        xacro_file,
+                    ]
+                )
+            }
+        ],
+        remappings=[
+            (
+                ("/tf", "tf"),
+                ("/tf_static", "tf_static"),
+            )
+        ],
     )
 
     # Launch the joint_state_publisher
     joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        output='screen',
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="screen",
         remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static'),
-        ]
+            ("/tf", "tf"),
+            ("/tf_static", "tf_static"),
+        ],
     )
 
-    return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_node
-    ])
+    return LaunchDescription([robot_state_publisher_node, joint_state_publisher_node])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     generate_launch_description()
