28f3be7f74573d25925cdb56ce9bda2085d7f919
Kai Dewey <deweykai5@gmail.com>
2024-04-02 00:04:28 -0600

    Update devcontainer to include up to date nav2 mppi controller

--- .devcontainer/build-webots.bash
+++ .devcontainer/build-webots.bash
@@ -4,6 +4,9 @@
 
 mkdir -p src
 git clone --recursive --depth 1 https://github.com/cyberbotics/webots_ros2.git src/webots_ros2
+git clone --recursive --depth 1 -b humble https://github.com/ros-planning/navigation2 src/navigation2
+mv src/navigation2/nav2_mppi_controller /src
+rm -rf src/navigation2
 apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
