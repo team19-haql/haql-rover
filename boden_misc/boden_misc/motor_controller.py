757a7dae5f0c7a870494811bf96b54ffe339679b
Kai Dewey <deweykai5@gmail.com>
2024-04-06 21:43:07 -0600

    fix i2c channel for orin nano to pcb connection

--- boden_misc/boden_misc/motor_controller.py
+++ boden_misc/boden_misc/motor_controller.py
@@ -3,23 +3,26 @@ import busio
 
 ADDR = 0x42
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.SCL_1, board.SDA_1)
+
 
 def read_motor_value(reg):
     buffer = bytearray(4)
 
     i2c.try_lock()
-    i2c.writeto_then_readfrom(ADDR,
+    i2c.writeto_then_readfrom(
+        ADDR,
         bytearray([reg]),
         buffer,
         out_start=0,
         out_end=4,
-        )
+    )
     i2c.unlock()
 
     value = int.from_bytes(buffer, byteorder='little', signed=True)
     value = value / (1 << 15)
     return value
+
 
 def write_motor_value(reg, value):
     value = int(value * (1 << 15))
@@ -28,3 +31,4 @@ def write_motor_value(reg, value):
     i2c.try_lock()
     i2c.writeto(ADDR, bytearray([reg]) + value)
     i2c.unlock()
+
