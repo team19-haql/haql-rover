4f58446e14ef0b897504375e3cf452e0786d40fb
Kai Dewey <deweykai5@gmail.com>
2024-04-19 14:36:23 -0600

    update motor controller from fixed to float

--- boden_misc/boden_misc/motor_controller.py
+++ boden_misc/boden_misc/motor_controller.py
@@ -1,8 +1,10 @@
+import struct
+
 import board
 import busio
-import time
 
 ADDR = 0x22
+
 
 def setup(alternate):
     global i2c
@@ -11,34 +13,38 @@ def setup(alternate):
     else:
         i2c = busio.I2C(board.SCL, board.SDA)
 
+
 def read_motor_value(reg):
     buffer = bytearray(4)
 
     i2c.try_lock()
-    i2c.writeto_then_readfrom(ADDR,
+    i2c.writeto_then_readfrom(
+        ADDR,
         bytearray([reg]),
         buffer,
         out_start=0,
         out_end=4,
-        )
+    )
     i2c.unlock()
 
-    value = int.from_bytes(buffer, byteorder='little', signed=True)
-    value = value / (1 << 15)
+    value = struct.unpack('<f', buffer)[0]
     return value
 
+
 def write_motor_value(reg, value):
-    value = int(value * (1 << 15))
-    value = value.to_bytes(4, byteorder='little', signed=True)
+    value = float(value)
+    value = struct.pack('<f', value)
 
     i2c.try_lock()
     i2c.writeto(ADDR, bytearray([reg]) + value)
     i2c.unlock()
 
+
 def scan_all():
     i2c.try_lock()
     print([hex(i) for i in i2c.scan()])
     i2c.unlock()
+
 
 if __name__ == '__main__':
     print('Bus 0')
